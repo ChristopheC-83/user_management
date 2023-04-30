d1cc52bedcd87ac4edbf2a941802ff5f988401fe
ChristopheC <93639544+ChristopheC-83@users.noreply.github.com>
2023-04-30 14:00:55 +0200

    Update pdo.model.php

--- models/pdo.model.php
+++ models/pdo.model.php
@@ -7,10 +7,10 @@ function setBDD()
     try {
         //connection à notre BDD, à modifier pour site en construction
         $pdo = new PDO(
-            'mysql:host=89.116.147.103;
-             dbname=u256533777_test',
-            'u256533777_christophec',
-            'Santa30420*',
+            'mysql:host=?????;
+             dbname==?????;',
+            '?????',
+            '?????',
             [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
         );
     } catch (PDOException $e) {
